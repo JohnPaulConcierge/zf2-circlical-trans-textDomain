bf83b1de972208eca0305ec52165c44303a61a26
Holger Maerz <grrompf@gmail.com>
2015-02-17 16:15:01 +0100

    Update TransFactory.php
    edited string

--- src/CirclicalTwigTrans/Factory/TransFactory.php
+++ src/CirclicalTwigTrans/Factory/TransFactory.php
@@ -1,33 +1,4 @@
 <?php
-
-/**
-,,
-`""*3b..											
-     ""*3o.					  						2/13/15 5:03 PM
-         "33o.			                  			S. Alexandre M. Lemaire
-           "*33o.
-              "333o.
-                "3333bo...       ..o:
-                  "33333333booocS333    ..    ,.
-               ".    "*3333SP     V3o..o33. .333b
-                "33o. .33333o. ...A33333333333333b
-          ""bo.   "*33333333333333333333P*33333333:
-             "33.    V333333333P"**""*"'   VP  * "l
-               "333o.433333333X
-                "*3333333333333AoA3o..oooooo..           .b
-                       .X33333333333P""     ""*oo,,     ,3P
-                      33P""V3333333:    .        ""*****"
-                    .*"    A33333333o.4;      .
-                         .oP""   "333333b.  .3;
-                                  A3333333333P
-                                  "  "33333P"
-                                      33P*"
-		                              .3"
-                                     "
-                                     
-                                     
-*/
-
 namespace CirclicalTwigTrans\Factory;
 
 use CirclicalTwigTrans\Model\Twig\Trans;
@@ -40,7 +11,7 @@ class TransFactory implements FactoryInterface
     {
         return new Trans(
             $serviceLocator->get('ZfcTwigRenderer'),
-            $serviceLocator->get('translator' )
+            $serviceLocator->get('MVCTranslator' )
         );
     }
 }
