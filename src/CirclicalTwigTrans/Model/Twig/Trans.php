19c0ac0e46786a4e2787d56093a46e320c68249e
Holger Maerz <grrompf@gmail.com>
2015-02-17 16:23:59 +0100

    Update Trans.php
    changed provided object for contructor introducing interface

--- src/CirclicalTwigTrans/Model/Twig/Trans.php
+++ src/CirclicalTwigTrans/Model/Twig/Trans.php
@@ -1,37 +1,7 @@
 <?php
-
-
-/**
-,,
-`""*3b..											
-     ""*3o.					  						11/11/13 12:40 PM
-         "33o.			                  			S. Alexandre M. Lemaire
-           "*33o.
-              "333o.
-                "3333bo...       ..o:
-                  "33333333booocS333    ..    ,.
-               ".    "*3333SP     V3o..o33. .333b
-                "33o. .33333o. ...A33333333333333b
-          ""bo.   "*33333333333333333333P*33333333:
-             "33.    V333333333P"**""*"'   VP  * "l
-               "333o.433333333X
-                "*3333333333333AoA3o..oooooo..           .b
-                       .X33333333333P""     ""*oo,,     ,3P
-                      33P""V3333333:    .        ""*****"
-                    .*"    A33333333o.4;      .
-                         .oP""   "333333b.  .3;
-                                  A3333333333P
-                                  "  "33333P"
-                                      33P*"
-		                              .3"
-                                     "
-                                     
-                                     
-*/
-
 namespace CirclicalTwigTrans\Model\Twig;
 
-use Zend\Mvc\I18n\Translator;
+use Zend\I18n\Translator\TranslatorInterface;
 
 class Trans extends \ZfcTwig\Twig\Extension
 {
@@ -48,7 +18,7 @@ class Trans extends \ZfcTwig\Twig\Extension
      *
      * @param Translator $trans
      */
-    public function __construct( \ZfcTwig\View\TwigRenderer $renderer, Translator $trans)
+    public function __construct( \ZfcTwig\View\TwigRenderer $renderer, TranslatorInterface $trans)
     {
         $this->renderer     = $renderer;
         $this->translator   = $trans;
