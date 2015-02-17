8e2af1fc8b62a544fd1261b81da59b7afaad8aeb
Holger Maerz <grrompf@gmail.com>
2015-02-17 16:29:26 +0100

    Update TransParser.php
    changed translator type

--- src/CirclicalTwigTrans/Model/Twig/TransParser.php
+++ src/CirclicalTwigTrans/Model/Twig/TransParser.php
@@ -1,47 +1,16 @@
 <?php
 
-
-
-/**
-,,
-`""*3b..											
-     ""*3o.					  						11/11/13 1:51 PM
-         "33o.			                  			S. Alexandre M. Lemaire
-           "*33o.
-              "333o.
-                "3333bo...       ..o:
-                  "33333333booocS333    ..    ,.
-               ".    "*3333SP     V3o..o33. .333b
-                "33o. .33333o. ...A33333333333333b
-          ""bo.   "*33333333333333333333P*33333333:
-             "33.    V333333333P"**""*"'   VP  * "l
-               "333o.433333333X
-                "*3333333333333AoA3o..oooooo..           .b
-                       .X33333333333P""     ""*oo,,     ,3P
-                      33P""V3333333:    .        ""*****"
-                    .*"    A33333333o.4;      .
-                         .oP""   "333333b.  .3;
-                                  A3333333333P
-                                  "  "33333P"
-                                      33P*"
-		                              .3"
-                                     "
-                                     
-                                     
-*/
-
-
 namespace CirclicalTwigTrans\Model\Twig;
 
-use Zend\Mvc\I18n\Translator;
+use Zend\I18n\Translator\TranslatorInterface;
 
 class TransParser extends \Twig_Extensions_TokenParser_Trans
 {
     protected $translator;
 
-    public function __construct( Translator $t )
+    public function __construct( TranslatorInterface $translator )
     {
-        $this->translator = $t;
+        $this->translator = $translator;
     }
 
     /**
